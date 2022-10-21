b6bbbb1bb907895a50b29b56c9cd8f257f5e234e
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2022-10-21 14:18:34 -0400

    modified truck trigger to always trigger when inside trigger box

--- Assets/Code/Level 3/TruckMoveTrigger.cs
+++ Assets/Code/Level 3/TruckMoveTrigger.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class TruckMoveTrigger : MonoBehaviour
 {
     public TruckController truck;
+    bool inTrigger = false;
 
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Please work");
         if(other.gameObject.CompareTag("Player")) {
+            // truck.move = true;
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            inTrigger = false;
+        }
+    }
+
+    void Update() {
+        if (inTrigger) {
             truck.move = true;
+            Debug.Log("in truck trigger");
         }
     }
 }
