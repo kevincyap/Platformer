ad52e7dcf8b6633f0281e2a31996bfaeb47a4277
Dahriel <dag642@nyu.edu>
2022-10-23 18:09:19 -0400

    Level 4 platforms working

---
+++ Assets/Code/Level 4/FallingPlatform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    GameObject player;
+    public float delay = 1f;
+    bool falling = false;
+    GameObject parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (falling)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
+            parent.transform.position = new Vector3(parent.transform.position.x, parent.transform.position.y + 0.005f, parent.transform.position.z);
+        }
+    }
+        // Update is called once per frame
+    //void OnTriggerEnter2D(Collider2D other){
+    //   if (other.CompareTag("Player"))
+    //    {
+    //        falling = true;
+    //        GameObject par = transform.parent.gameObject;
+    //        Destroy(parent, 5);
+    //    }
+    //}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        falling = true;
+        GameObject par = transform.parent.gameObject;
+        Destroy(parent, 5);
+    }
+}
